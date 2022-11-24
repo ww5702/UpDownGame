1ba7d7535755a281b4cac81c0a8cb69c0c3f2ed7
ios <wow5702@naver.com>
2022-11-24 17:29:43 +0900

    11.22.24 적중, 실패 시 컴퓨터가 지정한 랜덤변수 정답 공개 적중, 실패 시 사용자가 지정한 이름을 등수페이지에 기재 -> 실패 (버튼 액션 숙련도 미숙) 이름은 전달받았으나 alert 메시지와 동시에 구현하고 싶었기에 보류

--- UpDownGame/RankViewController.swift
+++ UpDownGame/RankViewController.swift
@@ -45,7 +45,7 @@ class RankViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     // @IBOutlet var tableView: UITableView!
     
-    // 텍스트 가져오는 문법
+    // main에서 텍스트 가져오기
     override func viewDidLoad() {
         if text != nil {
             label.text = text
